d8b1a7d5cbfaea2cce5b4e4c1e9c4110e51c9da9
Aleksei <a.gryczewski999@gmail.com>
2021-05-01 17:18:10 -0400

    Removed tooltip. Restored 64 stack.

---
+++ src/main/java/dev/salmonllama/cyf/items/foods/ApplePie.java
@@ -0,0 +1,10 @@
+package dev.salmonllama.cyf.items.foods;
+
+import net.minecraft.item.Item;
+
+public class ApplePie extends Item {
+
+    public ApplePie(Settings settings) {
+        super(settings);
+    }
+}
