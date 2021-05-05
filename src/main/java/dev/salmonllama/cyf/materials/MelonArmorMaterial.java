ad792c41312ee61b9b710edfec30603d790afb1b
Aleksei <a.gryczewski999@gmail.com>
2021-05-05 18:45:31 -0400

    Tweaked melon armor protection values.

--- src/main/java/dev/salmonllama/cyf/materials/MelonArmorMaterial.java
+++ src/main/java/dev/salmonllama/cyf/materials/MelonArmorMaterial.java
@@ -9,7 +9,7 @@ import net.minecraft.sound.SoundEvents;
 
 public class MelonArmorMaterial implements ArmorMaterial {
     private static final int[] BASE_DURABILITY = new int[] {13, 15, 16, 11};
-    private static final int[] PROTECTION_VALUES = new int[] {4, 7, 9, 4};
+    private static final int[] PROTECTION_VALUES = new int[] {3, 6, 7, 3};
 
     public static final MelonArmorMaterial INSTANCE = new MelonArmorMaterial();
 
