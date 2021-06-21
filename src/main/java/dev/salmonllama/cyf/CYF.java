9c9318c9f0a6c4d8635d74f2f26101eca47a6455
Aleksei <a.gryczewski999@gmail.com>
2021-06-21 17:00:27 -0400

    Added armor set effects and Saturation set effect for melon armor

--- src/main/java/dev/salmonllama/cyf/CYF.java
+++ src/main/java/dev/salmonllama/cyf/CYF.java
@@ -1,13 +1,25 @@
 package dev.salmonllama.cyf;
 
+import dev.salmonllama.cyf.items.CyfArmorItem;
 import dev.salmonllama.cyf.items.RegisterItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CYF implements ModInitializer {
+    public static final String MOD_ID = "cyf";
+    public static final String MOD_NAME = "cyf";
 
     public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
             new Identifier("cyf", "general"),
@@ -17,5 +29,36 @@ public class CYF implements ModInitializer {
     @Override
     public void onInitialize() {
         RegisterItems.register();
+    }
+
+    public static Map<CyfArmorMaterial, List<EquipmentSlot>> getModdedArmor(LivingEntity livingEntity) {
+        Map<CyfArmorMaterial, List<EquipmentSlot>> armor = new HashMap<>();
+        if (!(livingEntity instanceof PlayerEntity playerEntity)) { return armor; }
+
+        for (ItemStack itemStack : playerEntity.getInventory().armor) {
+            if (itemStack.getItem() instanceof CyfArmorItem item) {
+                CyfArmorMaterial material = item.getMaterial();
+                List<EquipmentSlot> equippedItems = armor.getOrDefault(material, new ArrayList<>());
+                equippedItems.add(item.getSlotType());
+                armor.put(material, equippedItems);
+            }
+        }
+
+        return armor;
+    }
+
+    @Nullable public static CyfArmorMaterial getPlayerMaterial(LivingEntity entity) {
+        if (!(entity instanceof PlayerEntity playerEntity)) { return null; }
+
+        CyfArmorMaterial material = null;
+
+        for (ItemStack piece : playerEntity.getInventory().armor) {
+            if (!(piece.getItem() instanceof CyfArmorItem armorItem)) { return null; }
+            CyfArmorMaterial mat = armorItem.getMaterial();
+            if (material == null) { material = mat; }
+            else if (material != mat) { return null; }
+        }
+
+        return material;
     }
 }
--- src/main/java/dev/salmonllama/cyf/materials/MelonArmorMaterial.java
+++ src/main/java/dev/salmonllama/cyf/materials/MelonArmorMaterial.java
@@ -1,13 +1,18 @@
 package dev.salmonllama.cyf.materials;
 
+import dev.salmonllama.cyf.CyfArmorMaterial;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.ArmorMaterial;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.world.World;
 
-public class MelonArmorMaterial implements ArmorMaterial {
+public class MelonArmorMaterial extends CyfArmorMaterial {
     private static final int[] BASE_DURABILITY = new int[] {13, 15, 16, 11};
     private static final int[] PROTECTION_VALUES = new int[] {3, 6, 7, 3};
 
@@ -51,5 +56,27 @@ public class MelonArmorMaterial implements ArmorMaterial {
     @Override
     public float getKnockbackResistance() {
         return 1.0F;
+    }
+
+    @Override
+    public int getSetBonuses() {
+        return 2;
+    }
+
+    @Override
+    public int getPieceBonuses() {
+        return 0;
+    }
+
+    @Override
+    public boolean isPartialSet() {
+        return false;
+    }
+
+    @Override
+    public void armorTick(World world, Entity entity, int count) {
+        StatusEffectInstance effect = new StatusEffectInstance(StatusEffects.SATURATION, 15, 2, false, true);
+        LivingEntity player = (LivingEntity) entity;
+        player.addStatusEffect(effect);
     }
 }
